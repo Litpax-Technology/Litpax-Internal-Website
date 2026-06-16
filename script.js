7c9de3cb396db5f1f6be68196b9eeb4adec79b46
mis-Darpan <mis@litpaxtechnology.com>
2026-06-16 16:25:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const DATA = {
     ['Bar Code Generation FMS','Barcode generation and production tracking system','https://docs.google.com/spreadsheets/d/17z88rv9VASDJy956wc-O3s7XV0qxscmuKdou2PR2PaM/edit?gid=0#gid=0','▣'],
     ['Generated Bar Code for Billing FMS','Generated barcode and billing management sheet','https://docs.google.com/spreadsheets/d/1QagqV8Oaxv_CTLgTBjroKlG7cJQ6nPuG5WYI5zERVuU/edit?gid=0#gid=0','▣'],
     ['2 Wheeler Production FMS','Production workflow and output tracking','https://docs.google.com/spreadsheets/d/1uaZOMO8n0mWBKsJAa4JtROKkU7Gq8sOTH1y6Cj83Cq0/edit?gid=811920351#gid=811920351','⇄'],
-    ['Dispatch to Delivery FMS','Dispatch and delivery workflow monitoring','https://docs.google.com/spreadsheets/d/171_8pxTY2inG933cMk2GmQKSihBqv3BIPZhpWoIn9JA/edit?gid=0#gid=0','⇄']
+    ['Production Slip FMS','Dispatch and delivery workflow monitoring','https://docs.google.com/spreadsheets/d/1fh6S6G9wwc9sKc4vbQmiFOHQZwa1RhNaYoIo2klo8Hw/edit?gid=1461042224#gid=1461042224','⇄']
   ],
 
   repairForms: [
