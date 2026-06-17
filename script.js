ff68aff1e2bb68e846a9c11ad7083525433afc6b
mis-Darpan <mis@litpaxtechnology.com>
2026-06-17 14:29:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,8 @@ const DATA = {
     ['Generated Bar Code for Billing FMS','Generated barcode and billing management sheet','https://docs.google.com/spreadsheets/d/1QagqV8Oaxv_CTLgTBjroKlG7cJQ6nPuG5WYI5zERVuU/edit?gid=0#gid=0','▣'],
     ['2 Wheeler Production FMS','Production workflow and output tracking','https://docs.google.com/spreadsheets/d/1uaZOMO8n0mWBKsJAa4JtROKkU7Gq8sOTH1y6Cj83Cq0/edit?gid=811920351#gid=811920351','⇄'],
     ['Production Slip FMS','Production Slip workflow monitoring','https://docs.google.com/spreadsheets/d/1fh6S6G9wwc9sKc4vbQmiFOHQZwa1RhNaYoIo2klo8Hw/edit?gid=1461042224#gid=1461042224','⇄'],
-    ['O to D FMS','Order to Delivery workflow monitoring','https://docs.google.com/spreadsheets/d/1Qnz-_Yqtj_GMd2s-z2BBRC9GH89UBBS0vdxLwzRfd-w/edit?gid=0#gid=0','⇄']
+    ['O to D FMS','Order to Delivery workflow monitoring','https://docs.google.com/spreadsheets/d/1Qnz-_Yqtj_GMd2s-z2BBRC9GH89UBBS0vdxLwzRfd-w/edit?gid=0#gid=0','⇄'],
+    ['Repair Dispatch Builty FMS','Repair Battery Dispatch Builty Tracking','https://docs.google.com/spreadsheets/d/189QqwxMvyzv6UTXJtDbOsPEHF7sFonn_ognI7nHr-Ns/edit?gid=278647415#gid=278647415','⇄']
   ],
 
   repairForms: [
